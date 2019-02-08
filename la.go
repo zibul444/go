1634ebdbbdcffd7346a15894dbcd52b4dcc6ab66
zibul444 <granj444@mail.ru>
2019-02-08 16:34:26 +0300

    ---

--- la.go
+++ la.go
@@ -2,10 +2,56 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(rand.NewSource(time.Now().UnixNano()))
+	//fmt.Println(rand.NewSource(time.Now().UnixNano()))
+	//a,s := 1, 2
+	//fmt.Println(incr(&a))
+	//fmt.Println(incr2(&s))
+	//incr2(&s)
+	//fmt.Println(a,s)
+
+	//t := Properties{}
+	//s := reflect.ValueOf(&t).Elem()
+	//typeOfT := s.Type()
+	//for i := 0; i < s.NumField(); i++ {
+	//	f := s.Field(i)
+	//	fmt.Printf("%d: %s %s = %v\n", i, typeOfT.Field(i).Name, f.Type(), f.Interface())
+	//}
+
+	s := "5"
+
+	var i int
+
+	//i = strconv.ParseInt(s, int, 16)
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(i)
+
+}
+
+func incr(i *int) int {
+	*i++
+	return *i
+}
+func incr2(i *int) {
+	*i++
+}
+
+type T struct {
+	A int
+	B string
+}
+
+type Properties struct {
+	Cycles  int
+	Res     float64
+	Size    int
+	Nframes int
+	Delay   int
 }
